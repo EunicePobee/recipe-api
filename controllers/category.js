568bb2fcc93d02e04ddfd40a2e7d506b98234bdd
EunicePobee <arabaawilba@gmail.com>
2024-06-28 15:51:44 +0000

    More on MongoDB

---
+++ controllers/category.js
@@ -0,0 +1,26 @@
+import { CategoryModel } from "../models/category.js";
+
+export const getCategories = async (req, res, next) => {
+    try {
+        // Get all categories from database
+        const allCategories = await CategoryModel.find();
+        // Return response
+        res.status(200).json(allCategories);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const postCategory = async (req, res, next) => {
+    try {
+        // Add a category to the database
+        const newCategory = await CategoryModel.create(req.body);
+        // Return response
+        res.status(201).json(newCategory);
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// Assignment - Write a function to change the favourite from false to true  
