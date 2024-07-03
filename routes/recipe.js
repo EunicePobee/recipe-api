742bb41fd89b1df0dac01b7604f058cf3cc338c0
EunicePobee <arabaawilba@gmail.com>
2024-07-03 12:32:20 +0000

    Remote File Upload

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { RecipeModel } from "../models/recipe.js";
 import { deleteRecipe, getRecipes, getSingleRecipe, patchRecipe, postRecipe } from "../controllers/recipe.js";
-import { localUpload } from "../middlewares/upload.js";
+import { localUpload, remoteUpload } from "../middlewares/upload.js";
 
 // Create router
 const recipeRouter = Router();
@@ -9,7 +9,7 @@ const recipeRouter = Router();
 // Define routes
 recipeRouter.get('/recipes', getRecipes);
 
-recipeRouter.post('/recipes', localUpload.single('image'), postRecipe);
+recipeRouter.post('/recipes', remoteUpload.single('image'), postRecipe);
 
 recipeRouter.patch('/recipes/:id', patchRecipe);
 
